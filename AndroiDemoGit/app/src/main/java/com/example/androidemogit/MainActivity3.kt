e4b51e921afd3d30f59e8b6ad251cc27ea350556
=khanh le <73209614+goodsolved99@users.noreply.github.com>
2020-11-04 21:26:20 +0700

    create mainActivity3

---
+++ AndroiDemoGit/app/src/main/java/com/example/androidemogit/MainActivity3.kt
@@ -0,0 +1,13 @@
+package com.example.androidemogit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        var fromTask1 = "aaggg"
+    }
+}
